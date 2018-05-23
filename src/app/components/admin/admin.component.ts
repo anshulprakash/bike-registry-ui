caeaae2dc3c7cdc6fb86e2fcd2decb4635b56c77
Anshul Prakash <anshul.prakash17@gmail.com>
2018-05-23 16:07:47 -0500

    Adding view bike and admin component

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -3,32 +3,7 @@ import { BikeService } from '../../services/bike.service';
 
 @Component({
   selector: 'app-admin',
-  template: `
-    <div class="container">
-      <table class="table table-bordered table-inverse">
-        <tr>
-          <th>Owner Name</th>
-          <th>Email</th>
-          <th>Model</th>
-          <th class="text-right">Purchase Price</th>
-        </tr>
-        <tr *ngFor="let bike of bikes">
-          <td>
-            {{bike.name}}
-          </td>
-          <td>
-            {{bike.email}}
-          </td>
-          <td>
-            {{bike.model}}
-          </td>
-          <td class="text-right">
-            {{bike.purchasePrice}}
-          </td>
-        </tr>
-      </table>
-    </div>
-  `,
+  templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
