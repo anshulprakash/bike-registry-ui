1988e8c6b217332c7c9655c66fd2451b25fd7530
Anshul Prakash <anshul.prakash17@gmail.com>
2018-05-23 14:31:34 -0500

    Intial commit

--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -13,6 +13,15 @@ export class BikeService {
   constructor(private http:HttpClient) { }
 
   getBikes(){
-    return this.http.get('/server/api/v1/bikes')
+    return this.http.get('/server/api/v1/bikes');
+  }
+
+  getBike(id : number){
+    return this.http.get('/server/api/v1/bikes/' + id);
+  }
+
+  createBikeRegistration(bike){
+    let body = JSON.stringify(bike);
+    return this.http.post('/server/api/v1/bikes', body, httpOptions);
   }
 }
